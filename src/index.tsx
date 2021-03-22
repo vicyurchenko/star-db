4ade1cd160a68b12b7f533f04d07e30a125c0c6c
Victor Yurchenko <vik@4vrs.com>
2021-03-22 15:53:10 +0700

    api client

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@
 
 const getResource = async (url: string) => {
     const res: Response = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
     const body: JSON = await res.json();
     return body;
 }
@@ -10,6 +13,9 @@ const getResource = async (url: string) => {
 getResource('https://swapi.dev/api/people/1/')
     .then((body) => {
         console.log(body);
-    });
+    })
+    .catch( (e) => {
+        console.error('Could not fetch', e);
+    } );
 
 export {}
