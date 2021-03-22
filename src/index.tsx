61388f59fbb03ba84898f81c0af186054740a5fb
Victor Yurchenko <vik@4vrs.com>
2021-03-22 14:15:16 +0700

    api get resource function

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
+const getResource = async (url: string) => {
+    const res: Response = await fetch(url);
+    const body: JSON = await res.json();
+    return body;
+}
 
+getResource('https://swapi.dev/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });
+
+export {}
