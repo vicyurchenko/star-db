8e4a3b3369eec64b162afdae5e37aa37fa571dff
Victor Yurchenko <vik@4vrs.com>
2021-03-26 18:22:12 +0700

    api get resource function

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 
+type responsePeopleData = {
+    birth_year: string,
+    created: string,
+    edited: string,
+    eye_color: string,
+    films: string[],
+    gender: string,
+    hair_color: string,
+    height: string,
+    homeworld: string,
+    mass: string,
+    name: string,
+    skin_color: string,
+    species: []
+    starships: string[],
+    url: string,
+    vehicles: string[]
+}
+
 class SwapiService {
 
     private apiBase: string = 'https://swapi.dev/api';
@@ -14,9 +33,9 @@ class SwapiService {
         return body;
     }
 
-    async getAllPeople(): Promise<JSON> {
-        const res: { results : JSON } = await this.getResource<{ results: JSON}>(`${this.apiBase}/people/`);
-        return res.results;
+    async getAllPeople(): Promise<Array<responsePeopleData>> {
+        const res: { results : Ar } = await this.getResource<{ results: string }>(`${this.apiBase}/people/`);
+        return res;
     }
 
     async getPerson(id: number): Promise<JSON> {
@@ -28,7 +47,14 @@ class SwapiService {
 
 const swapi: SwapiService = new SwapiService();
 
-swapi.getAllPeople().then( (body) => { console.log(body)})
+
+swapi.getAllPeople().then( (people) => {
+    console.log(typeof people);
+    /*const personObj = JSON.parse(people) as Array<responsePeopleData>;
+    personObj.forEach( (p)=> {
+        console.log(p.name);
+    });*/
+})
 
 
 export {}
