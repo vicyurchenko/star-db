f6609b22c345dec0978388d6f5b4290389b93da5
Victor Yurchenko <vik@4vrs.com>
2021-04-09 13:49:29 +0700

    error catching

---
+++ src/components/error-indicator/error-indicator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './error-indicator.css';
+
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <span className="boom">BOOM!</span>
+      <span>somthing has gone wrong</span>      
+      <span>(but we already sent droids to fix it)</span>
+    </div>
+  )
+}
+
+
+export default ErrorIndicator;
