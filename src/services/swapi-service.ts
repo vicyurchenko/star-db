49e121096ae10fc0b7d0c6892369ca38223b2fa8
Victor Yurchenko <vic.yurchenko@yandex.ru>
2021-06-03 00:25:33 +0700

    elint fixes

--- src/services/swapi-service.ts
+++ src/services/swapi-service.ts
@@ -79,7 +79,6 @@ export default class SwapiService {
   getResource = async <T>(url: string): Promise<T> => {
     console.log(url);
     const res: Response = await fetch(url);
-    console.log(res);
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, received ${res.status}`);
     }
@@ -110,7 +109,6 @@ export default class SwapiService {
 
   async getPlanet(id: number): Promise<planetData> {
     const res: responsePlanetData = await this.getResource<responsePlanetData>(`${this.apiBase}/planets/${id}`);
-    console.log('1234', res);
     return this.transformPlanet(res);
   }
 
